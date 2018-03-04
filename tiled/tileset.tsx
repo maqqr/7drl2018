a8b7f10b9fd41f38981b8d2cabca222a812bd1c2
GuruErkki <ekutvonen@gmail.com>
2018-03-04 23:38:48 +0200

    Added new map

--- tiled/tileset.tsx
+++ tiled/tileset.tsx
@@ -1,7 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="../static/tileset.png" width="256" height="256"/>
- <tile id="0" type="empty"/>
+ <tile id="0" type="empty">
+  <properties>
+   <property name="description" value="So much empty."/>
+  </properties>
+ </tile>
  <tile id="1" type="wall">
   <properties>
    <property name="description" value="This is a wall."/>
