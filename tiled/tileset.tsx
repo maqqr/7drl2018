d3d3abdf2b80882e9c508f4d4e6cc3b78da59974
GuruErkki <ekutvonen@gmail.com>
2018-03-04 19:54:24 +0200

    Removed propertiesmap, edited tileset.

--- tiled/tileset.tsx
+++ tiled/tileset.tsx
@@ -2,9 +2,119 @@
 <tileset name="tileset" tilewidth="16" tileheight="16" tilecount="256" columns="16">
  <image source="../static/tileset.png" width="256" height="256"/>
  <tile id="0" type="empty"/>
- <tile id="1" type="wall">
+ <tile id="1" type="wall1"/>
+ <tile id="2" type="wall2"/>
+ <tile id="3" type="wall3"/>
+ <tile id="4" type="wall4"/>
+ <tile id="5" type="floor1"/>
+ <tile id="6" type="floor2"/>
+ <tile id="7" type="floor3"/>
+ <tile id="8" type="floor4"/>
+ <tile id="9" type="door"/>
+ <tile id="10" type="dooropen"/>
+ <tile id="11" type="doormechanised"/>
+ <tile id="12" type="doormechanisedopen"/>
+ <tile id="13" type="water"/>
+ <tile id="14" type="lava"/>
+ <tile id="15" type="acid"/>
+ <tile id="25" type="doorbronzelocked">
   <properties>
-   <property name="solid" type="bool" value="true"/>
+   <property name="requireitem" value="keybronze"/>
+   <property name="useractivationtext" value="You open door."/>
   </properties>
  </tile>
+ <tile id="26" type="doorbronze">
+  <properties>
+   <property name="useractivationtext" value="You open door."/>
+  </properties>
+ </tile>
+ <tile id="27" type="doorbronzeopen"/>
+ <tile id="28" type="chest">
+  <properties>
+   <property name="useractivationtext" value="You open chest."/>
+  </properties>
+ </tile>
+ <tile id="29" type="chestopen"/>
+ <tile id="30" type="pentagram1"/>
+ <tile id="31" type="pentagram2"/>
+ <tile id="41" type="doorsilverlocked">
+  <properties>
+   <property name="requireitem" value="keysilver"/>
+   <property name="useractivationtext" value="You unlock door."/>
+  </properties>
+ </tile>
+ <tile id="42" type="doorsilver">
+  <properties>
+   <property name="useractivationtext" value="You open door."/>
+  </properties>
+ </tile>
+ <tile id="43" type="doorsilveropen"/>
+ <tile id="44" type="chestgold">
+  <properties>
+   <property name="useractivationtext" value="You open fancy chest."/>
+  </properties>
+ </tile>
+ <tile id="45" type="chestgoldopen"/>
+ <tile id="46" type="pentagram3"/>
+ <tile id="47" type="pentagram4"/>
+ <tile id="48" type="leverleft">
+  <properties>
+   <property name="activationtarget" value="[[0,0]]"/>
+   <property name="useractivationtext" value="You pull the lever."/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="activationtarget" value="[[0,0]]"/>
+   <property name="useractivationtext" value="You pull the lever."/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="activationtarget" value="[[0,0]]"/>
+  </properties>
+ </tile>
+ <tile id="51" type="pillar"/>
+ <tile id="52" type="statua"/>
+ <tile id="53" type="tomb"/>
+ <tile id="54" type="tombbroken"/>
+ <tile id="55" type="doorbar"/>
+ <tile id="56" type="doorbaropen"/>
+ <tile id="57" type="doorgoldlocked">
+  <properties>
+   <property name="requireitem" value="keygold"/>
+   <property name="useractivationtext" value="You unlock door."/>
+  </properties>
+ </tile>
+ <tile id="58" type="doorgold">
+  <properties>
+   <property name="useractivationtext" value="You open door."/>
+  </properties>
+ </tile>
+ <tile id="59" type="doorgoldopen"/>
+ <tile id="64" type="rock"/>
+ <tile id="66">
+  <properties>
+   <property name="activationtarget" value="[[0,0]]"/>
+  </properties>
+ </tile>
+ <tile id="67" type="pedestal"/>
+ <tile id="68" type="vase"/>
+ <tile id="69" type="table"/>
+ <tile id="70" type="tablebroken"/>
+ <tile id="71" type="tablevertical"/>
+ <tile id="72" type="rockpile"/>
+ <tile id="73" type="skullpile"/>
+ <tile id="74" type="statuebroken"/>
+ <tile id="80" type="trapspike"/>
+ <tile id="81" type="traspikeactivated"/>
+ <tile id="82" type="trapteleportin"/>
+ <tile id="83" type="trapteleportout"/>
+ <tile id="96" type="tunnelleftright"/>
+ <tile id="97" type="tunnelbottomup"/>
+ <tile id="98" type="tunnelbottomright"/>
+ <tile id="99" type="tunnelbottomleft"/>
+ <tile id="112" type="tunnelentrance"/>
+ <tile id="114" type="tunnelupright"/>
+ <tile id="115" type="tunnelleftup"/>
 </tileset>
