50f65db2d36919d7c91ea7f63c2e1ddb9959164d
Maqqr <rinne.simo@gmail.com>
2018-03-07 18:52:33 +0200

    Modified level loading to support different types of rooms

--- src/entity.ts
+++ src/entity.ts
@@ -1,4 +1,5 @@
 import { ICreature, IFurniture, IHasType, IItem, IPlayer, ITile } from "./interface/entity-schema";
+import { IPuzzleRoom } from "./interface/puzzle-schema";
 
 export class Entity<T extends IHasType> {
     public x: number;
@@ -10,18 +11,26 @@ export class Furniture extends Entity<IFurniture> {
     public offsetX: number;
     public offsetY: number;
 }
+
 export class Item extends Entity<IItem> {
 
 }
+
 export class Creature extends Entity<ICreature> {
     public currenthp: number;
     public willpower: number;
 
 }
+
 export class Player extends Entity<IPlayer> {
     public currentstability: number;
     public currentbody: Creature;
     public spiritpower: number;
     public spiritstability: number;
     public willpower: number;
+}
+
+export class PuzzleRoom {
+    public hasAppeared: boolean = false;
+    public dataRef: IPuzzleRoom;
 }
